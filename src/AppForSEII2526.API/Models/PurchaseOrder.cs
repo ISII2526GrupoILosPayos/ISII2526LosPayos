5ff0e56274e0b3c3a79647f300a2d00c834837f0
luismelero <lmelerojareno@gmail.com>
2025-09-22 17:20:52 +0200

    Adding attributes to PurchaseOrder

---
+++ src/AppForSEII2526.API/Models/PurchaseOrder.cs
@@ -0,0 +1,16 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class PurchaseOrder
+    {
+        public int Id { get; set; }
+
+        [StringLength(20, ErrorMessage = "Name of City can be neither longer than 20 characters nor shorter than 1", MinimumLength =1)]
+        public string City { get; set; }
+
+
+        [Display(Name = "Total price")]
+        [Precision(10, 2)]
+        public decimal TotalPrice { get; set; }
+       
+    }
+}
