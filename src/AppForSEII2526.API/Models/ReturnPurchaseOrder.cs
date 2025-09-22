c9b0ba72f56356c9703f01e99f70ca4fc5934251
Pau Femenia <pau.femenia@alu.uclm.es>
2025-09-22 17:20:08 +0200

    adding attributes for ReturnPurchaseOrder

---
+++ src/AppForSEII2526.API/Models/ReturnPurchaseOrder.cs
@@ -0,0 +1,24 @@
+﻿namespace AppForSEII2526.API.Models
+{
+
+    [Index(nameof(Name), IsUnique = true)]
+    public class ReturnPurchaseOrder
+    {
+        public int Id { get; set; }
+
+        [StringLength(20, ErrorMessage = "Maximun 20, minimun 10",MinimumLength =10)]
+        public string Name { get; set; }
+
+        [Precision(10, 2)]
+        public decimal TotalPrice { get; set; }
+
+        [Precision(10, 2)]
+        public decimal NewTotalPrice { get; set; }
+
+        [Precision(10, 2)]
+        public double MoneyToReturn { get; set; }
+
+
+
+    }
+}
