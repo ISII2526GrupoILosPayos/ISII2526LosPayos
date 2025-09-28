fb922ef0939d8f4bfa3ed659294b7dc2cbf532a8
luismelero <lmelerojareno@gmail.com>
2025-09-29 01:56:59 +0200

    Adding attributes tu PurchaseProduct class

---
+++ src/AppForSEII2526.API/Models/PurchaseProduct.cs
@@ -0,0 +1,26 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class PurchaseProduct
+    {
+        public int ProductID { get; set; }
+
+        [Precision(10, 2)]
+        public decimal Price { get; set; }
+
+        public int PurchaseProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is PurchaseProduct other)
+                return ProductID == other.ProductID;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ProductID.GetHashCode();
+        }
+    }
+}
