002a5acae9172c232bbae8be2b27954ec51d0f04
Hugo Hernández Gualda <hugo.hernandez2@alu.uclm.es>
2025-09-29 10:01:29 +0200

    Revert "Added PurchaseProducts y AppDbContext (#10)" (#17)
    No compilaba la DB.
    This reverts commit ca8a5eed96f2c7629198b3ac00a6ea1b60567187.

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,6 +6,4 @@ namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet<ReturnPurchaseOrder> ReturnPurchaseOrders { get; set; }
-    public DbSet<PurchaseProduct> PurchaseProducts { get; set; }
-
-}
+    }
