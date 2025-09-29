0c624908eb49091a339a86e0666b51aefc20115f
PauF2002 <146334569+PauF2002@users.noreply.github.com>
2025-09-29 10:31:20 +0200

    add ReturnProduct entity + DbSet  (#6)
    closes: #5
    * add ReturnProduct entity + DbSet + migration
    
    * Remove migration-Returnproduct
    
    ---------
    
    Co-authored-by: HUGO HERNÁNDEZ GUALDA <Hugo.Hernandez2@alu.uclm.es>

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,4 +6,5 @@ namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet<ReturnPurchaseOrder> ReturnPurchaseOrders { get; set; }
-    }
+    public DbSet<ReturnProduct> ReturnProducts { get; set; }
+}
