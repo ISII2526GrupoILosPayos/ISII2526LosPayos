ca8a5eed96f2c7629198b3ac00a6ea1b60567187
PauF2002 <146334569+PauF2002@users.noreply.github.com>
2025-09-29 09:20:07 +0200

    Added PurchaseProducts y AppDbContext (#10)
    closes: #9
    Co-authored-by: HUGO HERNÁNDEZ GUALDA <Hugo.Hernandez2@alu.uclm.es>

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,4 +6,6 @@ namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet<ReturnPurchaseOrder> ReturnPurchaseOrders { get; set; }
-    }
+    public DbSet<PurchaseProduct> PurchaseProducts { get; set; }
+
+}
