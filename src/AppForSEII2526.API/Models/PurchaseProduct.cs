5cafc6d4e07e470f84027b18563860839dc76a2e
luismelero <lmelerojareno@gmail.com>
2025-09-29 10:20:33 +0200

    Constructor and Required added

--- src/AppForSEII2526.API/Models/PurchaseProduct.cs
+++ src/AppForSEII2526.API/Models/PurchaseProduct.cs
@@ -1,26 +1,44 @@
-﻿namespace AppForSEII2526.API.Models
+﻿using Microsoft.CodeAnalysis;
+
+namespace AppForSEII2526.API.Models
 {
     public class PurchaseProduct
     {
-        public int ProductID { get; set; }
+        public PurchaseProduct() { }
 
+        public PurchaseProduct(int productId, int purchaseOrderId, decimal price, int quantity)
+        {
+            ProductId = productId;
+            PurchaseOrderId = purchaseOrderId;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        [Required]
         [Precision(10, 2)]
+        [Range(0, 9999999.99)]
         public decimal Price { get; set; }
 
-        public int PurchaseProductId { get; set; }
+        [Required]
+        public int ProductId { get; set; }
 
+        [Required]
+        public int PurchaseOrderId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
         public int Quantity { get; set; }
 
         public override bool Equals(object? obj)
         {
             if (obj is PurchaseProduct other)
-                return ProductID == other.ProductID;
+                return ProductId == other.ProductId;
             return false;
         }
 
         public override int GetHashCode()
         {
-            return ProductID.GetHashCode();
+            return ProductId.GetHashCode();
         }
     }
 }
