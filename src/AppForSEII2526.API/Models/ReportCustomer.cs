5e37ccf3aa68532b9459fa4810cddf6cc598a8d6
HUGO HERNÁNDEZ GUALDA <Hugo.Hernandez2@alu.uclm.es>
2025-10-01 17:21:31 +0200

    Add ReportCustomer class with attributes, hascode, constructos and equals.

---
+++ src/AppForSEII2526.API/Models/ReportCustomer.cs
@@ -0,0 +1,27 @@
+﻿
+namespace AppForSEII2526.API.Models
+{
+    public class ReportCustomer
+    {
+        public ReportCustomer()
+        {
+        }
+
+        public int BanReportId { get; set; }
+        public int CustomerId { get; set; }
+        public string Message { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ReportCustomer customer &&
+                   BanReportId == customer.BanReportId &&
+                   CustomerId == customer.CustomerId &&
+                   Message == customer.Message;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(BanReportId, CustomerId, Message);
+        }
+    }
+}
