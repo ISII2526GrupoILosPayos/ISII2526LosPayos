80b514fb435ec23d07a97dd148f0132bf73940cd
PauF2002 <146334569+PauF2002@users.noreply.github.com>
2025-10-02 13:35:34 +0200

    Add Relation Between ReturnProduct and PurchaseProduct and Adding Foreing Keys (#26)
    closes: #25
    * Add Relation Between ReturnProduct and PurchaseProduct
    
    * Corrección de texto ma puesto
    
    * Arreglada Relación Entre Purchase Products y ReturnProduct y merge local
    
    * Quitado Articulo inecesarioBrandId y añadidoRelacion IList a PurchaseProduct (DesdeProduct). Añadido PurchaseOrder para relaxion y Product Para relacion en(PurchasePorduct)
    
    * Comentado el Recurso de FK que no era necesario
    
    * Añadidio otra vez el FK de corrección

--- src/AppForSEII2526.API/Models/ReturnProduct.cs
+++ src/AppForSEII2526.API/Models/ReturnProduct.cs
@@ -1,8 +1,10 @@
-﻿namespace AppForSEII2526.API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema; // Asegúrate de tener este using
+
+namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(ProductId), nameof(PurchaseOrderId))]
     public class ReturnProduct
     {
-
         public ReturnProduct() { }
 
         public ReturnProduct(int quantity, string reason)
@@ -19,6 +21,13 @@
 
         public string Reason { get; set; } = string.Empty;
 
+        // Dependencia hacia PurchaseProduct
+        public int ProductId { get; set; }
+        public int PurchaseOrderId { get; set; }
+
+        [ForeignKey(nameof(ProductId) + "," + nameof(PurchaseOrderId))]
+        public PurchaseProduct PurchaseProduct { get; set; } = null!;
+
         public override bool Equals(object obj)
         {
             if (obj is ReturnProduct other)
@@ -30,10 +39,5 @@
         {
             return Id.GetHashCode();
         }
-
-        [Required]
-        public ReturnPurchaseOrder ReturnPurchaseOrder { get; set; }
-
-
     }
 }
