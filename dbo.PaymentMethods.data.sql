2e683baaec26253be426897a92eefd9e682c679a
Pau Femenia <pau.femenia@alu.uclm.es>
2025-10-13 18:21:42 +0200

    Adding ReturnProductController and BBDD information

---
+++ dbo.PaymentMethods.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[PaymentMethods] ON
+INSERT INTO [dbo].[PaymentMethods] ([Id], [UserId], [Discriminator], [TelephoneNumber], [CreditCardNumber], [ExpirationDate], [Email]) VALUES (3, N'1', N'none', 65432123, N'200000000', N'2050-12-12 00:00:00', N'pau@gmail.com')
+SET IDENTITY_INSERT [dbo].[PaymentMethods] OFF
---
+++ src/AppForSEII2526.API/Controllers/ReturnProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReturnProductsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ReturnProductsController> _logger;
+
+        public ReturnProductsController(ApplicationDbContext context, ILogger<ReturnProductsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //[Route("[action]")]
+        //[ProducesResponseType(typeof(decimal), (int) HttpStatusCode.OK)]
+        //[ProducesResponseType(typeof(string), (int) HttpStatusCode.BadRequest)]
+        //public async Task<ActionResult>  ComputeDivision(decimal op1, decimal op2)
+        //{
+        //    if (op2== 0)
+        //    {
+        //        string error = "OP2 cannot be 0 to compute a division";
+        //        _logger.LogError(DateTime.Now + "Error" + error);
+        //        return BadRequest(error);
+        //    }
+        //   decimal result =  op1/op2;
+        //    return Ok(result);
+        //}
+
+
+    }
+}
