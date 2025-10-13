a9a505d1ced1ffa63e8a00d9e6c5d9b5a612d8a3
luismelero-7 <Luis.melero1@alu.uclm.es>
2025-10-13 23:25:34 +0200

    Database of Products and Brand

---
+++ src/AppForSEII2526.API/Data/dbo.Brands.data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[Brands] ON
+INSERT INTO [dbo].[Brands] ([Id], [Name], [Location]) VALUES (1, N'H&S', N'Madrid')
+INSERT INTO [dbo].[Brands] ([Id], [Name], [Location]) VALUES (2, N'Bezoya', N'Burgos')
+SET IDENTITY_INSERT [dbo].[Brands] OFF
---
+++ src/AppForSEII2526.API/Data/dbo.Products.data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[Products] ON
+INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (10, N'Shampoo', N'New', N'White', CAST(5.00 AS Decimal(10, 2)), 100, 0, 1)
+INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (11, N'Water', N'Smart', N'Blue', CAST(1.20 AS Decimal(10, 2)), 100, 0, 2)
+SET IDENTITY_INSERT [dbo].[Products] OFF
