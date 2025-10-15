e91f1b0790e2dcb89689d363256be049f55c35d2
HUGO HERNÁNDEZ GUALDA <Hugo.Hernandez2@alu.uclm.es>
2025-10-15 11:19:04 +0200

    Creating controller and dto

---
+++ src/AppForSEII2526.API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using AppForSEII2526.API.DTOs.UserDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ApplicationDbContext context, ILogger<UsersController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<UserForBanDTO>), (int) HttpStatusCode.OK)]
+        public async Task<IActionResult> GetUsers(string? surname, ComplaintType? complaintType)
+        {
+            IList<UserForBanDTO> usersDTO = await _context.ApplicationUsers
+                .Where(m=> m.Surname.Contains(surname) || (surname==null))
+                .Select(m=> new UserForBanDTO(m.Id,
+                    m.Name
+                ))
+                .ToListAsync();
+            return Ok(usersDTO);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/UserDTOs/UserForBanDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace AppForSEII2526.API.DTOs.UserDTOs
+{
+    public class UserForBanDTO
+    {
+        public UserForBanDTO(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //public UserForBanDTO(string id, string name, string surname, DateTime accountCreationDate, int numberOfComplaints, IList<ComplaintType> complaintTypes, bool complaintProcessed)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    Surname = surname;
+        //    AccountCreationDate = accountCreationDate;
+        //    NumberOfComplaints = numberOfComplaints;
+        //    ComplaintTypes = complaintTypes;
+        //    ComplaintProcessed = complaintProcessed;
+        //}
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        //public DateTime AccountCreationDate { get; set; }
+        //public int NumberOfComplaints { get; set; }
+        //public IList<ComplaintType> ComplaintTypes { get; set; }
+        //public bool ComplaintProcessed { get; set; }
+
+    }
+}
