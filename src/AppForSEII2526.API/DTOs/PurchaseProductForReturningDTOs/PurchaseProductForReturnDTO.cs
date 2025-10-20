f56fa877a5fa2319b8ac929f6ba8130f8011afda
Pau Femenia <pau.femenia@alu.uclm.es>
2025-10-20 16:50:31 +0200

    Añadido Location, Stock, PurchaseOrderID a DTO

--- src/AppForSEII2526.API/DTOs/PurchaseProductForReturningDTOs/PurchaseProductForReturnDTO.cs
+++ src/AppForSEII2526.API/DTOs/PurchaseProductForReturningDTOs/PurchaseProductForReturnDTO.cs
@@ -2,19 +2,28 @@
 {
     public class PurchaseProductForReturnDTO
     {
-        public PurchaseProductForReturnDTO(int id, string name, string brandName)
+        public PurchaseProductForReturnDTO(int id, string name, string brand, int stock, string location)
         {
             Id = id;
             Name = name;
-            Brand = brandName;
+            Brand = brand;
+            Stock = stock;
+            Location = location;
         }
 
         public int Id { get; set; }
+
+        public int PurchaseOrderId { get; set; }
 
         //Nombre del Producto a Devolver
         [StringLength(50, ErrorMessage = "Name of the Product cannot be longer than 50 characters")]
         public string Name { get; set; }
 
         public string Brand { get; set; }
-        }
-}
+
+        public int Stock { get; set; }
+
+        public string Location { get; set; }
+    }
+
+    }
