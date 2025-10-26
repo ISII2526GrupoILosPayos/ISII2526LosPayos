61adab357850f7c31fe18bdc61e0f2e6b846e2b7
luismelero-7 <Luis.Melero1@alu.uclm.es>
2025-10-26 19:14:30 +0100

    Developing the SelectProductsForPurchase (#83)
    closes: #80
    * Controller created
    
    * Database of Products and Brand
    
    * Get method from the controller made and DTOs folder created. I've also started the ProductForPurchaseDTO
    
    * Data of the products shown in the api added
    
    * Filter by colour and not found clause added

---
+++ src/AppForSEII2526.API/DTOs/ProductDTOs/ProductForPurchaseDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace AppForSEII2526.API.DTOs.ProductDTOs
+{
+    public class ProductForPurchaseDTO
+    {
+        public ProductForPurchaseDTO(int id, string name, string brandName, string location, int stock)
+        {
+            Id = id;
+            Name = name;
+            Brand = brandName;
+            Location = location;
+            Stock = stock;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Location { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
--- src/AppForSEII2526.API/Data/dbo.Products.data.sql
+++ src/AppForSEII2526.API/Data/dbo.Products.data.sql
@@ -1,4 +1,6 @@
 ﻿SET IDENTITY_INSERT [dbo].[Products] ON
 INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (4, N'Camiseta', N'Camiseta', N'Blue', CAST(15.00 AS Decimal(10, 2)), 10, 1, 1)
 INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (5, N'Pantalon', N'Pantalon', N'Negro', CAST(30.00 AS Decimal(10, 2)), 30, 1, 2)
+INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (10, N'Shampoo', N'New', N'White', CAST(5.00 AS Decimal(10, 2)), 100, 0, 3)
+INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (11, N'Water', N'Smart', N'Blue', CAST(1.20 AS Decimal(10, 2)), 100, 0, 4)
 SET IDENTITY_INSERT [dbo].[Products] OFF
