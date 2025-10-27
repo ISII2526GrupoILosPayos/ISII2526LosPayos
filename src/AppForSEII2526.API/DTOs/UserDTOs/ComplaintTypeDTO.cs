694a6266d7b20c97dd8cb27f79e7c06f05f04927
Hugo Hernández Gualda <hugo.hernandez2@alu.uclm.es>
2025-10-27 17:28:17 +0100

    82 develop selectuserforban (#88)
    closes: #82
    * Creating controller and dto
    
    * Developing getUsers controller
    
    * Finishing coding GetUser function.
    
    * Finishing implementing the getUsers controller.

---
+++ src/AppForSEII2526.API/DTOs/UserDTOs/ComplaintTypeDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace AppForSEII2526.API.DTOs.UserDTOs
+{
+    public class ComplaintTypeDTO
+    {
+        public ComplaintTypeDTO(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+
+        public string Name { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/UserDTOs/UserForBanDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace AppForSEII2526.API.DTOs.UserDTOs
+{
+    public class UserForBanDTO
+    {
+        public UserForBanDTO(string id, string name, string surname, DateTime accountCreationDate, IList<ComplaintTypeDTO> complaintTypes)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            AccountCreationDate = accountCreationDate;
+            ComplaintTypes = complaintTypes;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime AccountCreationDate { get; set; }
+        public IList<ComplaintTypeDTO> ComplaintTypes { get; set; }
+
+    }
+}
