e6023b3f1d5dc7c8d98893e1be55b640423bf2a7
HUGO HERNÁNDEZ GUALDA <Hugo.Hernandez2@alu.uclm.es>
2025-10-29 20:25:28 +0100

    Starting to develop

---
+++ src/AppForSEII2526.API/Controllers/BanReportController.cs
@@ -0,0 +1,42 @@
+﻿using AppForSEII2526.API.DTOs.BanReportDTO;
+using AppForSEII2526.API.DTOs.UserDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BanReportController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<UsersController> _logger;
+
+        public BanReportController(ApplicationDbContext context, ILogger<UsersController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<BanReportResultDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetBanReport(int id)
+        {
+            IList<BanReportResultDTO> banReportsDTO = await _context.BanReports
+                .Where(m => m.Id == id)
+                .Include(n => n.ReportCustomers)
+                .ThenInclude(o => o.Customer)
+                .ThenInclude(p => p.Complaint)
+                .Select(q => new BanReportResultDTO(
+                    q.ReportCustomers.Custo
+
+                ))
+                .ToListAsync();
+            return Ok(banReportsDTO);
+        }
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/BanReportDTO/BanReportResultDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace AppForSEII2526.API.DTOs.BanReportDTO
+{
+    public class BanReportResultDTO
+    {
+
+        public BanReportResultDTO(string name, string surname, string message, string reason, string description, DateTime startDate, DateTime endDate){
+            this.name = name;
+            this.surname = surname;
+            this.message = message;
+            this.reason = reason;
+            this.description = description;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string message { get; set; }
+        public string reason { get; set; }
+        public string description { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+    }
+}
