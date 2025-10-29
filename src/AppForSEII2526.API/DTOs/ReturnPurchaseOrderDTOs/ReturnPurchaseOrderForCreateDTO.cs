54075ccd608a0c341eb0823c6aa5f0e0bf45d1e8
Pau Femenia <pau.femenia@alu.uclm.es>
2025-10-29 11:18:54 +0100

    Medio hecho las pruebas de unidad

---
+++ src/AppForSEII2526.API/DTOs/ReturnPurchaseOrderDTOs/ReturnPurchaseOrderForCreateDTO.cs
@@ -0,0 +1,63 @@
+﻿namespace AppForSEII2526.API.DTOs.ReturnPurchaseOrderDTOs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    namespace AppForSEII2526.API.DTOs.ReturnProductDTOs
+    {
+        public class ReturnPurchaseOrderForCreateDTO
+        {
+            public ReturnPurchaseOrderForCreateDTO(
+                string customerUserName,
+                string customerName,
+                string customerSurname,
+                string customerAddress,
+                string customerTelephoneNumber,
+                string paymentMethod,   // "credit card", "paypal", "other"
+                string reason,          // motivo devolución
+                int? rating,            // 1..5 opcional
+                IList<ReturnProductForCreateDTO> productsToReturn
+            )
+            {
+                CustomerUserName = customerUserName;
+                CustomerName = customerName;
+                CustomerSurname = customerSurname;
+                CustomerAddress = customerAddress;
+                CustomerTelephoneNumber = customerTelephoneNumber;
+                PaymentMethod = paymentMethod;
+                Reason = reason;
+                Rating = rating;
+                ProductsToReturn = productsToReturn;
+            }
+
+            [Required]
+            public string CustomerUserName { get; set; } // quién está devolviendo (similar a CustomerUserName en rental)
+
+            [Required]
+            public string CustomerName { get; set; }     // Name
+
+            [Required]
+            public string CustomerSurname { get; set; }  // Surname (solo uno, como decidimos)
+
+            [Required]
+            public string CustomerAddress { get; set; }  // Address
+
+            [Required]
+            public string CustomerTelephoneNumber { get; set; } // Telephone number
+
+            [Required]
+            public string PaymentMethod { get; set; }    // credit card / paypal / other
+
+            [Required]
+            public string Reason { get; set; }           // motivo obligatorio del paso 5
+
+            [Range(1, 5)]
+            public int? Rating { get; set; }             // opcional
+
+            [Required]
+            public IList<ReturnProductForCreateDTO> ProductsToReturn { get; set; }
+        }
+    }
+
+}
---
+++ test/AppForSEII2526.UT/PurchaseProductForReturnController_test/GetPurchaseProductForReturning_test.cs
@@ -0,0 +1,77 @@
+﻿using AppForSEII2526.API.Controllers;
+using AppForSEII2526.API.DTOs.ReturnProductDTOs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace AppForSEII2526.UT.PurchaseProductForReturnController_test
+{
+    public class GetPurchaseProductForReturning_test : AppForSEII25264SqliteUT
+    {
+        public GetPurchaseProductForReturning_test()
+        {
+            var purchaseProduct = new List<AppForSEII2526.API.Models.PurchaseProduct>
+            {
+                new AppForSEII2526.API.Models.PurchaseProduct
+                {
+                    ProductId = 1,
+                    PurchaseOrderId = 1,
+                    Price = 100,
+                    Quantity = 2
+                },
+                new AppForSEII2526.API.Models.PurchaseProduct
+                {
+                    ProductId = 2,
+                    PurchaseOrderId = 1,
+                    Price = 200,
+                    Quantity = 1
+                }
+
+
+            };
+
+            var purchaseOrder = new AppForSEII2526.API.Models.PurchaseOrder
+            {
+                City = "Albacete",
+                TotalPrice = 10,
+                Date = DateTime.Now,
+                Description = "VeryGood",
+                NameSurname = "Fernandez",
+                PostalCode = "02002",
+                Street = "Campus",
+                Rating = 3,
+                State = AppForSEII2526.API.Models.PurchaseState.Done,
+                ApplicationUserId = "1"
+            };
+
+            var user = new AppForSEII2526.API.Models.ApplicationUser
+            {
+                Name = "Pau",
+                Surname = "Femenia",
+                Address = "Campus",
+                AccountCreationDate = DateTime.Now
+            };
+
+
+            _context.ApplicationUsers.Add(user);
+            _context.PurchaseOrders.Add(purchaseOrder);
+            _context.PurchaseProducts.AddRange(purchaseProduct);
+            _context.SaveChanges();
+
+        }
+        [Fact]
+        public async Task GetPurchaseProductForReturning_null_PP_PO_AU()
+        {
+            IList<PurchaseProductForReturnDTO> purchaseproductForReturn = new IList<PurchaseProductForReturnDTO> ()
+            {
+
+            }
+        }
+
+}
