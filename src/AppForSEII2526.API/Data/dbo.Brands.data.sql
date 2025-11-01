9a930fc255d5d6d66255b6b6fc2bfd56ace130e3
luismelero-7 <Luis.melero1@alu.uclm.es>
2025-11-01 11:41:18 +0100

    Database uploaded and Products controller fixed, but this branch is not for products controller

--- src/AppForSEII2526.API/Data/dbo.Brands.data.sql
+++ src/AppForSEII2526.API/Data/dbo.Brands.data.sql
@@ -1,6 +1,5 @@
 ﻿SET IDENTITY_INSERT [dbo].[Brands] ON
-INSERT INTO [dbo].[Brands] ([Id], [Name], [Location]) VALUES (1, N'Zara', N'Albacete')
-INSERT INTO [dbo].[Brands] ([Id], [Name], [Location]) VALUES (2, N'Pull&Bear', N'Albacete')
-INSERT INTO [dbo].[Brands] ([Id], [Name], [Location]) VALUES (3, N'H&S', N'Madrid')
-INSERT INTO [dbo].[Brands] ([Id], [Name], [Location]) VALUES (4, N'Bezoya', N'Burgos')
+INSERT INTO [dbo].[Brands] ([Id], [Name], [Location]) VALUES (1, N'H&S', N'Madrid')
+INSERT INTO [dbo].[Brands] ([Id], [Name], [Location]) VALUES (2, N'Bezoya', N'Burgos')
+INSERT INTO [dbo].[Brands] ([Id], [Name], [Location]) VALUES (5, N'Sony', N'USA')
 SET IDENTITY_INSERT [dbo].[Brands] OFF
