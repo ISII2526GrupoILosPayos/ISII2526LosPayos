0290165aff3182d3247ea742f76e07e84c182e18
luismelero-7 <Luis.Melero1@alu.uclm.es>
2025-11-04 14:21:03 +0100

    Developing GetPurchaseOrder (#95)
    closes: #87
    * GetPurchaseOrder almost finished until some corrections
    
    * I have tried to configurate the getPurchaseOrder method for when id is null, all the purchase are returned
    
    * Where clause fixed
    
    * Database uploaded and Products controller fixed, but this branch is not for products controller
    
    * fixed

--- src/AppForSEII2526.API/Data/dbo.PaymentMethods.data.sql
+++ src/AppForSEII2526.API/Data/dbo.PaymentMethods.data.sql
@@ -1,5 +1,7 @@
 ﻿
 SET IDENTITY_INSERT [dbo].[PaymentMethods] ON
 INSERT INTO [dbo].[PaymentMethods] ([Id], [UserId], [Discriminator], [TelephoneNumber], [CreditCardNumber], [ExpirationDate], [Email]) VALUES (3, N'1', N'Bizum', 65432123, N'200000000', N'2050-12-12 00:00:00', N'pau@gmail.com')
+INSERT INTO [dbo].[PaymentMethods] ([Id], [UserId], [Discriminator], [TelephoneNumber], [CreditCardNumber], [ExpirationDate], [Email]) VALUES (4, N'1', N'none', 602453356, N'200000111', N'2032-08-17 00:00:00', N'luis@gmail.com')
+INSERT INTO [dbo].[PaymentMethods] ([Id], [UserId], [Discriminator], [TelephoneNumber], [CreditCardNumber], [ExpirationDate], [Email]) VALUES (5, N'2', N'none', 666777889, N'200000125', N'2029-03-25 00:00:00', N'kykybusiness@gmail.com')
 SET IDENTITY_INSERT [dbo].[PaymentMethods] OFF
 
