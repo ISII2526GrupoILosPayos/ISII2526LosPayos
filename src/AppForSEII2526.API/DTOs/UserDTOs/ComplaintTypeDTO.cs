006e6427c745f7c12341908cd193710b88899fee
Hugo Hernández Gualda <hugo.hernandez2@alu.uclm.es>
2025-11-14 22:16:28 +0100

    92 develop getusersforban testing (#112)
    * Empezando los tests
    
    * Generar equals method
    
    * Get users for ban testing

--- src/AppForSEII2526.API/DTOs/UserDTOs/ComplaintTypeDTO.cs
+++ src/AppForSEII2526.API/DTOs/UserDTOs/ComplaintTypeDTO.cs
@@ -10,5 +10,12 @@
 
         public string Name { get; set; }
         public int Count { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ComplaintTypeDTO dTO &&
+                   Name == dTO.Name &&
+                   Count == dTO.Count;
+        }
     }
 }
--- src/AppForSEII2526.API/DTOs/UserDTOs/UserForBanDTO.cs
+++ src/AppForSEII2526.API/DTOs/UserDTOs/UserForBanDTO.cs
@@ -1,4 +1,5 @@
-﻿namespace AppForSEII2526.API.DTOs.UserDTOs
+﻿
+namespace AppForSEII2526.API.DTOs.UserDTOs
 {
     public class UserForBanDTO
     {
@@ -16,6 +17,39 @@
         public string Surname { get; set; }
         public DateTime AccountCreationDate { get; set; }
         public IList<ComplaintTypeDTO> ComplaintTypes { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not UserForBanDTO other)
+                return false;
+
+            if (Id != other.Id ||
+                Name != other.Name ||
+                Surname != other.Surname ||
+                AccountCreationDate != other.AccountCreationDate)
+                return false;
+
+            if (ComplaintTypes.Count != other.ComplaintTypes.Count)
+                return false;
+
+            for (int i = 0; i < ComplaintTypes.Count; i++)
+            {
+                if (!ComplaintTypes[i].Equals(other.ComplaintTypes[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = HashCode.Combine(Id, Name, Surname, AccountCreationDate);
+
+            foreach (var ct in ComplaintTypes)
+                hash = HashCode.Combine(hash, ct.GetHashCode());
+
+            return hash;
+        }
 
     }
 }
