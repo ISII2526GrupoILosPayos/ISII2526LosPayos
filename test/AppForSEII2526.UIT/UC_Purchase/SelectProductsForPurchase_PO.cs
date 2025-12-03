025aab66f4bc65a2647f039bfa84263f9276815d
luismelero-7 <Luis.melero1@alu.uclm.es>
2025-12-03 10:41:31 +0100

    Creating the page object to interact with selectProductsForPurchase

---
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectProductsForPurchase_PO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Purchase
+{
+    public class SelectProductsForPurchase_PO : PageObject
+    {
+        public SelectProductsForPurchase_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+    }
+}
