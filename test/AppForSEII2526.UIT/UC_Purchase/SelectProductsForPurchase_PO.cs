e1dcd4d00459d8fd131c888444a2dc79714698c7
luismelero-7 <Luis.melero1@alu.uclm.es>
2025-12-03 10:58:22 +0100

    Creating the page object to intereact with the web elements

--- test/AppForSEII2526.UIT/UC_Purchase/SelectProductsForPurchase_PO.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectProductsForPurchase_PO.cs
@@ -8,8 +8,17 @@ namespace AppForSEII2526.UIT.UC_Purchase
 {
     public class SelectProductsForPurchase_PO : PageObject
     {
+        By productName = By.Id("inputName");
+        By productColour = By.Id("inputColour");
+        By buttonSearchProducts = By.Id("searchProducts");
         public SelectProductsForPurchase_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
+        }
+        public void SearchProducts(string name)
+        {
+            WaitForBeingClickable(productName);
+            _driver.FindElement(productName).SendKeys(name);
+            _driver.FindElement(buttonSearchProducts).Click();
         }
     }
 }
