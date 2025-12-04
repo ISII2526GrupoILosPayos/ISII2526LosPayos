b5f2a7a7e1404ed3dc12e9101ff05c029fedeaa2
Pau Femenia <pau.femenia@alu.uclm.es>
2025-12-04 16:32:16 +0100

    He añadido la información necesaria en AaplicationUser Para crea un usuario nuevo

--- src/AppForSEII2526.Web/Data/ApplicationUser.cs
+++ src/AppForSEII2526.Web/Data/ApplicationUser.cs
@@ -5,5 +5,9 @@ namespace AppForSEII2526.Web.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public string Name { get; set; }
+    public string? Surname { get; set; }
+    public string? Address { get; set; }
+    public DateTime AccountCreationDate { get; set; }
 }
 
