8432f0f7e9f1a5dddfddb1f2f5ca3c09e3fdcc18
luismelero-7 <Luis.melero1@alu.uclm.es>
2025-12-13 12:56:53 +0100

    We add the method for filling all the fields in the createPurchase page

--- test/AppForSEII2526.UIT/UC_Purchase/CreatePurchase_PO.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/CreatePurchase_PO.cs
@@ -19,5 +19,31 @@ namespace AppForSEII2526.UIT.UC_Purchase
         public CreatePurchase_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         { 
         }
+
+        public void FillInPurchaseInfo(string nameSurname, string userName, string deliveryAddress, string city, string postalCode, string paymentMethod)
+        {
+            // Esperamos a que el formulario esté visible
+            WaitForBeingVisible(_nameSurnameBy);
+
+            // Rellenamos los campos de texto
+            _nameSurname().Clear();
+            _nameSurname().SendKeys(nameSurname);
+
+            _userName().Clear();
+            _userName().SendKeys(userName);
+
+            _deliveryAddress().Clear();
+            _deliveryAddress().SendKeys(deliveryAddress);
+
+            _city().Clear();
+            _city().SendKeys(city);
+
+            _postalCode().Clear();
+            _postalCode().SendKeys(postalCode);
+
+            // Seleccionamos el método de pago (dropdown)
+            SelectElement selectElement = new SelectElement(_paymentMethod());
+            selectElement.SelectByText(paymentMethod);
+        }
     }
 }
