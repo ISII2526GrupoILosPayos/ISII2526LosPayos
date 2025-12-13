d57a7bd91ce10cf7038def3d4f5a98134efbc256
luismelero-7 <Luis.Melero1@alu.uclm.es>
2025-12-13 01:32:40 +0100

    Develop uit for SelectProductsForPurchase - Test cases UC77_4 - UC77_6 (#151)
    closes: #130
    * using the API to obtain products
    
    * appsettings fixed
    
    * Adding the filters
    
    * including the PurchaseStateContainer
    
    * allowing to select the products for being purchased
    
    * error
    
    * updating the nuget and the port in UC_UIT
    
    * Created the class for implementing UIT
    
    * Creating the page object to interact with selectProductsForPurchase
    
    * Creating the page object to intereact with the web elements
    
    * For colour
    
    * id modified for being the same
    
    * we introduce the filter by Colour in the PageObject
    
    * we check whether the table contains the expected data
    
    * we support the precondition of our UseCase Login
    
    * we add the id to the option of the menue we want to select
    
    we support the intial steps: login, and click on rental
    
    * we develop the first filter: filter by name
    
    * we transform our testcase in DataDrive Test using Theory
    
    * SeedData created
    
    * we automate testcase UC2_5
    
    * Correction for tests working
    
    * Starting the development of the test case UC77_6
    
    * automating UC77_6 (purchase not available**)
    
    * changing name

---
+++ test/AppForSEII2526.UIT/UC_Purchase/UC_PurchaseProducts_UIT.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.UIT.Shared;
+
+namespace AppForSEII2526.UIT.UC_Purchase
+{
+    public class UC_PurchaseProducts_UIT : UC_UIT
+    {
+        private SelectProductsForPurchase_PO selectProductsForPurchase_PO;
+        private const int productId1 = 1;
+        private const string productName1 = "PS5";
+        private const string productBrand1 = "Sony";
+        private const string productLocation1 = "USA";
+        private const string productQuantity1 = "10";
+
+        private const string productName2 = "Pantalon";
+        private const string productBrand2 = "Pull&Bear";
+        private const string productLocation2 = "Albacete";
+        private const string productQuantity2 = "30";
+
+        public UC_PurchaseProducts_UIT(ITestOutputHelper output) : base(output)
+        {
+            selectProductsForPurchase_PO = new SelectProductsForPurchase_PO(_driver, _output);
+        }
+
+        private void Precondition_perform_login()
+        {
+            Perform_login("Luis.melero1@alu.uclm.es", "Password1234%");
+        }
+
+        private void InitialStepsForPurchaseProducts()
+        {
+            Precondition_perform_login();
+            //we wait for the option of the menu to be visible
+            selectProductsForPurchase_PO.WaitForBeingVisible(By.Id("CreatePurchaseOrder"));
+            //we click on the menu
+            _driver.FindElement(By.Id("CreatePurchaseOrder")).Click();
+        }
+
+        [Theory]
+        [InlineData(productName1, productBrand1, productLocation1, productQuantity1, "PS5", "")]
+        [InlineData(productName2, productBrand2, productLocation2, productQuantity2, "", "Negro")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC77_AF1_UC77_4_5filtering(string productName, string productBrand, string productLocation, string productQuantity,
+            string filterName, string filterColour)
+        {
+            //Arrange
+            InitialStepsForPurchaseProducts();
+            var expectedProducts = new List<string[]> { new string[] { productName, productBrand, productLocation, productQuantity }, };
+
+            //Act
+            selectProductsForPurchase_PO.SearchProducts(filterName, filterColour);
+
+            //Assert
+
+            Assert.True(selectProductsForPurchase_PO.CheckListOfProducts(expectedProducts));
+
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+
+        public void UC77_AF3_UC77_6_RentingNotavailable()
+        {
+            //Arrange
+            InitialStepsForPurchaseProducts();
+            //Act
+
+            //We search products for being able to add some products to the cart
+            selectProductsForPurchase_PO.SearchProducts("", "");
+
+            selectProductsForPurchase_PO.AddProductToPurchaseCart(productName1);
+            selectProductsForPurchase_PO.RemoveProductFromPurchaseCart(productName1);
+
+            //Assert
+
+            Assert.True(selectProductsForPurchase_PO.PurchaseNotAvailable());
+
+        }
+    }
+}
