c5eef1e516511b547a9d5ca90c78a7592573eb65
HUGO HERNÁNDEZ GUALDA <Hugo.Hernandez2@alu.uclm.es>
2026-03-01 17:22:59 +0100

    Fixing selectuserforban_PO

--- test/AppForSEII2526.UIT/UC_BanReport/SelectUserForBanReport_PO.cs
+++ test/AppForSEII2526.UIT/UC_BanReport/SelectUserForBanReport_PO.cs
@@ -1,139 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-
-
-namespace AppForSEII2526.UIT.UC_BanReport
+﻿namespace AppForSEII2526.UIT.UC_BanReport
 {
     public class SelectUsersForBan_PO : PageObject
     {
-        private readonly By _surnameBy = By.Id("surnameFilter");
-        private readonly By _complaintTypeBy = By.Id("complaintTypeFilter");
-        private readonly By _creationDateBy = By.Id("creationDateFilter");
+        By buttonSearchUsers = By.Id("searchUsers");
+        By buttonContinue = By.Id("Continue");
+        By surnameFilter = By.Id("surnameFilter");
+        By complaintFilter = By.Id("complaintTypeFilter");
+        By creationDate = By.Id("creationDateFilter");
+        By tableUsers = By.Id("TableOfUsersForBan");
+        By showSelectedUsers = By.Id("showBanCart");
+        By tableSelectedUsers = By.Id("TableOfSelectedUsers");
 
-        private readonly By _showBanCartBy = By.Id("showBanCart");
-        private readonly By _searchUsersBy = By.Id("searchUsers");
-        private readonly By _continueBy = By.Id("Continue");
 
-        private readonly By _tableOfUsersBy = By.Id("TableOfUsersForBan");
-        private readonly By _tableOfSelectedUsersBy = By.Id("TableOfSelectedUsers");
 
-        private IWebElement Surname() => _driver.FindElement(_surnameBy);
-        private IWebElement ComplaintType() => _driver.FindElement(_complaintTypeBy);
-        private IWebElement CreationDate() => _driver.FindElement(_creationDateBy);
+        IWebElement _continueButton() => _driver.FindElement(buttonContinue);
 
-        private IWebElement ShowBanCartButton() => _driver.FindElement(_showBanCartBy);
-        private IWebElement SearchUsersButton() => _driver.FindElement(_searchUsersBy);
-        private IWebElement ContinueButton() => _driver.FindElement(_continueBy);
+        public SelectUsersForBan_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output) { }
 
-        public SelectUsersForBan_PO(IWebDriver driver, ITestOutputHelper output)
-            : base(driver, output)
+        public void searchUser(string surname, string type)
         {
+            WaitForBeingClickable(surnameFilter);
+            _driver.FindElement(surnameFilter).Clear();
+            _driver.FindElement(surnameFilter).SendKeys(surname);
+
+            _driver.FindElement(complaintFilter).Clear();
+            _driver.FindElement(complaintFilter).SendKeys(surname);
+
+            _driver.FindElement(buttonSearchUsers).Click();
         }
 
-        public void FilterUsers(string? surnameFilter, string? complaintTypeFilter, DateTime? creationDate)
+        public bool CheckListOfUsers(List<string[]> expectedUsers)
         {
-            WaitForBeingVisible(_surnameBy);
 
-            Surname().Clear();
-            if (!string.IsNullOrWhiteSpace(surnameFilter))
-                Surname().SendKeys(surnameFilter);
-
-            ComplaintType().Clear();
-            if (!string.IsNullOrWhiteSpace(complaintTypeFilter))
-                ComplaintType().SendKeys(complaintTypeFilter);
-
-            CreationDate().Clear();
-            if (creationDate.HasValue)
-            {
-                InputDateInDatePicker(_creationDateBy, creationDate.Value);
-            }
-
-            SearchUsersButton().Click();
-
-            Thread.Sleep(2000);
+            return CheckBodyTable(expectedUsers, tableUsers);
         }
 
-        public void SelectUsersByIds(List<string> userIds)
+        public void AddUserToBanReport(string userNumber)
         {
-            foreach (var id in userIds)
-            {
-                var addBtn = By.Id($"userToBan_{id}");
-                WaitForBeingVisible(addBtn);
-                _driver.FindElement(addBtn).Click();
-            }
+            WaitForBeingClickable(By.Id("userToBan_" + userNumber));
+
+            _driver.FindElement(By.Id("userToBan_" + userNumber)).Click();
         }
 
-        public void SelectFirstUsers(int numberOfUsers)
+        public void RemoveUserFromBanReport(string userNumber)
         {
-            WaitForBeingVisible(_tableOfUsersBy);
 
-            var table = _driver.FindElement(_tableOfUsersBy);
-
-            var addButtons = table.FindElements(By.CssSelector("tbody button[id^='userToBan_']")).ToList();
-
-            for (int i = 0; i < numberOfUsers && i < addButtons.Count; i++)
-                addButtons[i].Click();
+            WaitForBeingClickable(showSelectedUsers);
+            _driver.FindElement(showSelectedUsers).Click();
+            _driver.FindElement(By.Id("removeUser_" + userNumber)).Click();
         }
 
-        public void ToggleBanCart()
+        public bool ContinueNotAvailable()
         {
-            WaitForBeingClickable(_showBanCartBy);
-            ShowBanCartButton().Click();
+            //the button is not Displayed=hidden
+
+            return _driver.FindElement(buttonContinue).Displayed == false;
         }
 
-        public void ModifyBanCart_RemoveUser(string customerId)
+        public bool ContinueAvailable()
         {
-            ToggleBanCart();
-
-            var removeBtn = By.Id($"removeUser_{customerId}");
-            WaitForBeingVisible(removeBtn);
-            _driver.FindElement(removeBtn).Click();
+            return _driver.FindElement(buttonContinue).Displayed;
         }
 
-        public void ContinueToCreateBanReport()
+        public void Continue()
         {
-            WaitForBeingClickable(_continueBy);
-            ContinueButton().Click();
-        }
-
-        public bool CheckContinueDisabled()
-        {
-            return !ContinueButton().Enabled;
-        }
-
-        public int GetUsersCountInTable()
-        {
-            if (!_driver.FindElements(_tableOfUsersBy).Any())
-                return 0;
-
-            var table = _driver.FindElement(_tableOfUsersBy);
-            return table.FindElements(By.CssSelector("tbody tr")).Count;
-        }
-
-        public bool CheckSelectedUsersCount(int expected)
-        {
-            ToggleBanCart();
-
-            if (!_driver.FindElements(_tableOfSelectedUsersBy).Any())
-                return expected == 0;
-
-            var table = _driver.FindElement(_tableOfSelectedUsersBy);
-            var rows = table.FindElements(By.CssSelector("tbody tr"));
-            return rows.Count == expected;
-        }
-
-        public bool CheckUsersTableContainsRow(string userId)
-        {
-            var row = By.Id($"UserData_{userId}");
-            return _driver.FindElements(row).Any();
-        }
-
-        public bool CheckSelectedUserRowExists(string customerId)
-        {
-            var row = By.Id($"SelectedUser_{customerId}");
-            return _driver.FindElements(row).Any();
+            WaitForBeingClickable(buttonContinue);
+            _continueButton().Click();
         }
     }
 }
